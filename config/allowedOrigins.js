32be3621b618c7795f513e895b48eee336d7a320
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-11-13 18:33:22 +0000

    Deployment ready

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+	/* 'http://localhost:3000', */
+	'https://angeltech.onrender.com',
+];
 
 module.exports = allowedOrigins;
